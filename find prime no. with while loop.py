7ac190532b85a31a61ba6ab4501cca19bd47e227
KAUSHAL CHAUHAN <93530886+kaushalch5287@users.noreply.github.com>
2022-03-12 22:49:46 +0530

    Update find prime no. with while loop.py

--- find prime no. with while loop.py
+++ find prime no. with while loop.py
@@ -1,8 +1,13 @@
 #with while loop to find prime no.
-
 n = int(input("enter the number: "))
-while n%n == 0:
-    print("prime")
-    break 
+count =0
+while n > 0:
+    for i in range(1,n+1):
+        if n%i == 0:
+         count += 1
+    break
+if count == 2:
+    print("prime number")
 else:
     print("not prime")
+ 
